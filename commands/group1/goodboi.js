36305f4bc12486c30da3324b59d75d3ec19e218d
alex <davis.alex.jr@gmail.com>
2018-10-20 17:15:10 -0400

    removed reply

---
+++ commands/group1/goodboi.js
@@ -0,0 +1,17 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class GoodboiCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'who\'s a goodboi?',
+            group: 'group1',
+            memberName: 'goodboi',
+            description: 'Is Argos a goodboi!',
+            examples: ['goodboi']
+        });
+    }
+
+    run(msg) {
+        return msg.say('Everyone is a goodboi!');
+    }
+};
---
+++ readme.md
@@ -0,0 +1,1 @@
+I'm a dog and I love everything!
